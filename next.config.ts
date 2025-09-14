17cfc69e560aa5220a927b1526d89b44673387ea
Sahil Rezoan <sahilrezoan246@gmail.com>
2025-09-14 19:31:54 -0400

    chore: skip ESLint during build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // Skip ESLint during `next build` (Vercel uses this)
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
