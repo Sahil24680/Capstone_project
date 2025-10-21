8e84023cd86b9bcbced940822d85c50a3e9a9cac
she-who-shall-not-be-named <aliciaisler26@gmail.com>
2025-10-21 17:44:38 -0400

    update

--- lib/nlp.ts
+++ lib/nlp.ts
@@ -1,5 +1,1 @@
-// nlp.ts
-
-import {} from '../lib/nlp/client';
-import {} from '../lib/db/jobFeatures';
-import {} from '../lib/db/jobUpdates';
+// lib/nlp.ts
--- lib/scraper.ts
+++ lib/scraper.ts
@@ -1,11 +1,11 @@
 // lib/scraper.ts
-import type { AdapterJob } from "../adapters/types";
-import { greenhouseAdapter } from "../adapters/greenhouse";
+import type { AdapterJob } from "../lib/adapters/types";
+import { greenhouseAdapter } from "../lib/adapters/greenhouse";
 import {
   extractWebFeaturesFromJsonLd,
   extractWebFeaturesFromText,
-} from "../normalizers/web";
-import { canFetchUrl } from "../scoring/robots";
+} from "../lib/normalizers/web";
+import { canFetchUrl } from "./robots";
 
 /**
  * Strict denylist for aggregator/marketing boards; add as needed
