43f51cdc6e85ee7de2730f02e01bb01211aef0e0
she-who-shall-not-be-named <aliciaisler26@gmail.com>
2025-10-20 22:34:04 -0400

    WIP: pre-rebase snapshot

--- lib/scoring/adapters.ts
+++ lib/scoring/adapters.ts
@@ -1,6 +1,6 @@
 // lib/adapters.ts
-import { greenhouseAdapter } from "./adapters/greenhouse";
-import { webAdapter } from "./adapters/web";
+ import { greenhouseAdapter } from "../adapters/greenhouse";
+import { webAdapter } from "../adapters/web";
 
 type GreenhouseFn = (tenant_slug: string, external_job_id: string) => Promise<any>;
 type WebFn = (url: string) => Promise<any>;
